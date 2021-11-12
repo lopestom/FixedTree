ecb690f761a3ec2ececbe65849f4866260764820
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-12 20:39:24 +0000

    Update omni_Wasp.mk

--- omni_Wasp.mk
+++ omni_Wasp.mk
@@ -2,10 +2,10 @@
 PRODUCT_RELEASE_NAME := Wasp
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := nokia

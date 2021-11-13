f5960ec66bbcb67370546c165a680bab59962c8f
lopestorm <lopestom1@gmail.com>
2021-11-13 15:12:55 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-for var in eng user userdebug; do
-  add_lunch_combo omni_Wasp-$var
-done
+#for var in eng user userdebug; do
+#  add_lunch_combo omni_Wasp-$var
+#done
